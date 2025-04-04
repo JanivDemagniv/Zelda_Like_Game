53e925c95edc078b0322c3b49b7073996912e044
JanivDemagniv <janivjry@gmail.com>
2025-04-04 12:52:46 +0300

    first initial

---
+++ code/level.py
@@ -0,0 +1,30 @@
+import pygame
+from setting import *
+from tile import Tile
+from player import Player
+
+class Level:
+    def __init__(self):
+        #display surface
+        self.display_surface = pygame.display.get_surface()
+        
+        #sprites groups setups
+        self.visable_sprites = pygame.sprite.Group()
+        self.obstacles_sprites = pygame.sprite.Group()
+
+        #sprite setup
+        self.create_map()
+
+    def create_map(self):
+        for row_index,row in enumerate(WORLD_MAP):
+            for col_index,col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x,y),[self.visable_sprites,self.obstacles_sprites])
+                if col == 'p':
+                    Player((x,y),[self.visable_sprites])
+
+    def run(self):
+        #update and draw the gaem
+        self.visable_sprites.draw(self.display_surface)
