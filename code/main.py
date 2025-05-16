77a4c4b062a99bb67c8253546d110815d96122d2
JanivDemagniv <janivjry@gmail.com>
2025-05-16 15:38:46 +0300

    general music sound reducing

--- code/main.py
+++ code/main.py
@@ -16,6 +16,7 @@ class Game:
         #sound
         main_sound = pygame.mixer.Sound('audio/main.ogg')
         main_sound.play(loops= -1)
+        main_sound.set_volume(0.5)
 
     def run(self):
         while True:
